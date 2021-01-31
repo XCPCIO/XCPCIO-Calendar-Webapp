16648acf0c3095bd0218378fcae9d6784264b35f
Dup4 <lyuzhi.pan@gmail.com>
2021-01-31 20:58:36 +0800

    feat: init

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'umi';
+import CONFIG from './config';
 
 export default defineConfig({
     nodeModulesTransform: {
         type: 'none',
     },
+    title: false,
+    metas: CONFIG.metas,
+    favicon: CONFIG.favicon,
+    analytics: CONFIG.analytics,
+    hash: true,
     routes: [{ path: '/', component: '@/pages/index' }],
     fastRefresh: {},
 });
---
+++ config.ts
@@ -0,0 +1,21 @@
+const config = {
+    title: 'Calendar - XCPCIO',
+    favicon: '/favicon.png',
+    publicPath: '/',
+    analytics: {
+        ga: '',
+        baidu: '',
+    },
+    metas: [
+        {
+            name: 'keywords',
+            content: 'icpc, ccpc, calendar',
+        },
+        {
+            name: 'description',
+            content: 'XCPCIO-Calendar 主要收录竞赛日历。',
+        },
+    ],
+};
+
+export default config;
